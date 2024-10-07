54cd8e9dbecb28c6e5527d10bad6b5908a4130bc
Sonu-Pandey1 <sonupandeybaldeo2003@gmail.com>
2024-10-07 11:58:25 +0530

    connected to db

---
+++ Backend/conf/db/dbConfig.js
@@ -0,0 +1,16 @@
+
+const mongoose = require("mongoose");
+
+const dbConnect = async ()=>{
+
+    try {
+        const connection = await  mongoose.connect(process.env.MONGODB_URI);
+        console.log("mongoDb Connected Successfully.")
+        
+    } catch (error) {
+        console.log("mongoDb Connection Error:",error)
+        
+    }
+}
+
+module.exports = dbConnect;
--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
 const express = require("express");
 
+const dbConnect = require("./conf/db/dbConfig")
 
 const app = express();
-let PORT =process.env.PORT || 5000
+let PORT = process.env.PORT || 5000
 
 app.use(express.json());
+
+
+
+
+// connect to mongoDB
+dbConnect();
 
 
 
